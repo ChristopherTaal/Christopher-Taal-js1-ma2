cc278a9ccd20be217115d90fb4281a7714a894c1
ChristopherTaal <63666767+ChristopherTaal@users.noreply.github.com>
2020-08-23 23:51:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// question 1
+const button = document.getElementById("gb");
+
+button.onclick = function () {
+    document.body.style.background ="green";
+};
+// question 2
+const button2 = document.querySelector(".toggler");
+const toggleBox = document.querySelector(".togglee”");
+button2.onclick = function() {
+    toggleBox.classList.toggle("extra");
+    if (toggleBox.className === "togglee extra" ){
+        toggleBox.innerHTML = "Extra";
+    }
+    else{
+        toggleBox.innerHTML = "Toggle my class.";
+    }
+}
+
+//question 3
+
+const dropdown = document.querySelector("select");
+const paragraphsContainer = document.querySelector(".paragraphs-container");
+
+dropdown.onchange = function () {
+
+    paragraphsContainer.innerHTML="";
+    let selectedValue = this.value;
+    for(let i = 1; i <= selectedValue; i++) {
+        paragraphsContainer.innerHTML += `<div class="paragraphs-container"></div>`;
+    }
+
+};
+
+//question 4
+
+const message = document.querySelector(".input");
+const characterCount = document.querySelector(".char-count b");
+
+message.onkeyup = function () {
+    console.log(event.target.value.length);
+
+    const len = event.target.value.length;
+
+    characterCount.innerHTML = len;
+};
+//question 5
+    function handleScroll() {
+    const scrolledY = window.scrollY;
+    if (scrolledY > 35) {
+        document.body.classList.add("highlight");
+    }
+    else {
+        document.body.classList.remove("highlight");
+    }
+    }
+window.onscroll = handleScroll;
